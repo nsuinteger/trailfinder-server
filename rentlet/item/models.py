da6e847633da0a529c8304596d75221e4c5960b9
Nguyen Hoang Yen <HoangYen@Nguyens-MacBook-Pro.local>
2014-12-06 18:05:06 +0800

    add item list

--- rentlet/item/models.py
+++ rentlet/item/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    photo_link = models.CharField(max_length=200)
--- rentlet/item/views.py
+++ rentlet/item/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django.shortcuts import render
+
+# Create your views here.
+from item.models import Item
+from jsonutil import json_success
+
+
+def item_list(request):
+    item_list = Item.objects.all()
+    json_data = {}
+    json_data['item_list'] = []
+    for item in item_list:
+        json_data['item_list'].append({
+            "item_name": "item1"
+        })
+    return json_success(request, json_data)
