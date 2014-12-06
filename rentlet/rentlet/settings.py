c85b6929e5278d49207d4f794c31af6d59503bc3
Mevan 2359 <mevan.alles@gmail.com>
2014-12-06 19:23:18 +0800

    settings update

--- rentlet/rentlet/settings.py
+++ rentlet/rentlet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ALLOWED_HOSTS = ['https://frozen-mesa-5439.herokuapp.com']
 
 
 # Quick-start development settings - unsuitable for production
